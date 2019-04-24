4902a694a6f50a3c9cab2cd1ea1ed1e90aaab614
zackzheng <zackzheng@meijiabang.cn>
2019-04-24 19:12:04 +0800

    first commit

---
+++ Test/Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Zack･Zheng on 2019/4/24.
+//  Copyright © 2019 Zack･Zheng. All rights reserved.
+//
+
+import AdSupport
+import BusinessChat
+import CarPlay
+import CoreData
+import DispatchIntrospection
+import MediaToolbox
+import SQLite3
+import UIKit
+import zlib
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ ZExtension/SourceEditorCommand.swift
@@ -0,0 +1,45 @@
+//
+//  SourceEditorCommand.swift
+//  ZExtension
+//
+//  Created by Zack･Zheng on 2019/4/24.
+//  Copyright © 2019 Zack･Zheng. All rights reserved.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+
+        let linesToSort = invocation.buffer.lines.filter { line in
+            return (line as? String)?.hasPrefix("import") ?? false
+        }
+        
+        guard linesToSort.count > 0 else {
+            completionHandler(nil)
+            return
+        }
+        
+        let firstLineIndex = invocation.buffer.lines.index(of: linesToSort[0]) // For insert
+        guard firstLineIndex >= 0 else {
+            completionHandler(nil)
+            return
+        }
+        invocation.buffer.lines.removeObjects(in: linesToSort)
+        let linesSorted = (linesToSort as? [String] ?? []).sorted() {$0 <= $1}
+        linesSorted.reversed().forEach { (line) in
+            invocation.buffer.lines.insert(line, at: firstLineIndex)
+        }
+        let selectionsUpdated: [XCSourceTextRange] = (0..<linesSorted.count).map { (index) in
+            let lineIndex = firstLineIndex + index
+            let endColumn = linesSorted[index].count - 1
+            return XCSourceTextRange(start: XCSourceTextPosition(line: lineIndex, column: 0), end: XCSourceTextPosition(line: lineIndex, column: endColumn))
+        }
+        invocation.buffer.selections.setArray(selectionsUpdated)
+        completionHandler(nil)
+    }
+    
+}
